f7e90763a196c22dc18718d15813b1db82799f37
maimeimow <134258119+maimeimow@users.noreply.github.com>
2023-07-18 14:52:26 +0900

    release

--- data-platform-storage-bin-sql-general-data.sql
+++ data-platform-storage-bin-sql-general-data.sql
@@ -4,7 +4,7 @@ CREATE TABLE `data_platform_storage_bin_general_data`
    `Plant`                        varchar(4) NOT NULL,
    `StorageLocation`              varchar(4) NOT NULL, 
    `StorageBin`                   varchar(10) NOT NULL,
-   `StorageType`                  varchar(200) DEFAULT NULL,
+   `StorageBinDescription`        varchar(200) DEFAULT NULL,
    `XCoordinates`                 float(10) DEFAULT NULL,
    `YCoordinates`                 float(10) DEFAULT NULL,
    `ZCoordinates`                 float(10) DEFAULT NULL,
@@ -12,14 +12,15 @@ CREATE TABLE `data_platform_storage_bin_general_data`
    `CapacityUnit`                 varchar(3) DEFAULT NULL,
    `CapacityWeight`               float(10) DEFAULT NULL,
    `CapacityWeightUnit`           varchar(3) DEFAULT NULL,
+   `CreationDate`                 date NOT NULL,
+   `LastChangeDate`               date NOT NULL,
+   `IsMarkedForDeletion`          tinyint(1) DEFAULT NULL,
 
     PRIMARY KEY (`BusinessPartner`, `Plant`, `StorageLocation`, `StorageBin`),
 
     CONSTRAINT `DataPlatformStorageBinDataStorageLocation_fk` FOREIGN KEY (`BusinessPartner`, `Plant`, `StorageLocation`) REFERENCES `data_platform_plant_storage_location_data` (`BusinessPartner`, `Plant`, `StorageLocation`),
     CONSTRAINT `DataPlatformStorageBinDataCapacityUnit_fk` FOREIGN KEY (`CapacityUnit`) REFERENCES `data_platform_quantity_unit_quantity_unit_data` (`QuantityUnit`),
     CONSTRAINT `DataPlatformStorageBinDataCapacityWeightUnit_fk` FOREIGN KEY (`CapacityWeightUnit`) REFERENCES `data_platform_quantity_unit_quantity_unit_data` (`QuantityUnit`)
-
-
 
 ) ENGINE=InnoDB
   DEFAULT CHARSET=utf8mb4;
